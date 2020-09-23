56f44e8ac87dd50afc8870dd3d7c3a54b8c72497
Your Name <you@example.com>
2020-09-23 17:02:27 +0100

    react-app

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style ={{marginBottom: "5px"}}   >
+            <div class="card-footer text-muted">
+            now
+            </div>
+        </div>
+    )
+}
+
+export default Footer
